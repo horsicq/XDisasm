b7abe8ae22f0f35028aee42f3ceaa1b3db0be8d3
horsicq <horsicq@gmail.com>
2020-06-15 02:48:34 +0200

    Fix

--- xdisasm.cpp
+++ xdisasm.cpp
@@ -67,10 +67,9 @@ void XDisasm::_disasm(qint64 nInitAddress, qint64 nAddress)
 
             XBinary::_zeroMemory(opcode,N_X64_OPCODE_SIZE);
 
-            XBinary::read_array(pDevice,nOffset,opcode,N_X64_OPCODE_SIZE); // TODO defs
+            size_t nDataSize=XBinary::read_array(pDevice,nOffset,opcode,N_X64_OPCODE_SIZE); // TODO defs
 
             uint8_t *pData=(uint8_t *)opcode;
-            size_t nDataSize=N_X64_OPCODE_SIZE;
 
             cs_insn *insn;
             size_t count=cs_disasm(disasm_handle,pData,nDataSize,nAddress,1,&insn);
